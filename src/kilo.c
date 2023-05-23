4e755f84448dca56242de539538890d8fd7b3d30
famous31 <famousalele@gmail.com>
2023-05-23 05:34:52 +0100

    delete executeable

--- src/kilo.c
+++ src/kilo.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+// #include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
+// global variable to store the original state of the terminal before we edit it's 
+// variable values. This allows us to restore the terminal 
+// state to it's original value at program inception
+struct termios org_termios;
+
+//
+void disbleRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 //this function turns off terminal echo. 
 //On default the terminal is in 'cooked mode' or 'canonical mode'
@@ -10,9 +21,21 @@
 //to do this, we use the tcgetattr() function to read the current 
 //attributes into a struct and write the terminal attributes back out.
 void enableRawMode(){
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
+    tcgetattr(STDIN_FILENO, &org_termios);
+    struct termios raw = org_termios;
+    atexit(disbleRawMode);
+
+    // the ICANON flag (2 in decimal or 0b 0000 0010) allows us to turn off the canonical flag and 
+    // read input from STDIN byte by byte
+    // the ECHO flag is (decimal 8, or byte 0b 0000 1000)
+    // when we bit OR the ECHO and ICANON flags, the resulting binary is 
+    // a number with both the 2nd and 4th bit set to 1. --> 0b 0000 1010
+    // the '~' sign would allow us to switch off the fourth bit and second bit,
+    // while keeping the other bits on. Bit AND with any number would preserve the bit value i the 
+    // other operand except for the 4th and 2nd digit
+    // Bit AND works like ===> 1111 & 1011 == 1011, 1011001 & 1111111 ===> 1011001
+    // and then we can use that to mask the variables in the c_lfag variable
+    raw.c_lflag &= ~(ECHO | ICANON); 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
